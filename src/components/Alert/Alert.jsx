ef3522c40a319c19e9f0768fb5c3f38e417e3e3d
Brandon Williams <brando5393@gmail.com>
2025-05-23 01:50:32 +0000

    update alert.jsx

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-const Alert = (props) => {
-  let styles = {
-    display: "none",
-  };
-  {
-    props.showAlert
-      ? (styles = { display: "block" })
-      : (styles = { display: "none" });
-  }
-  return (
-    <div className="alert alert-danger" role="alert" style={styles}>
-      Can't submit empty search, please try again.
-    </div>
-  );
-};
+const Alert = ({ showAlert }) => (
+  <div
+    className="alert alert-danger"
+    role="alert"
+    style={{ display: showAlert ? "block" : "none" }}
+  >
+    Can't submit empty search, please try again.
+  </div>
+);
 
 export default Alert;
