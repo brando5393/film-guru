0810c41e1e03ce543f576f43ea954503b0365c86
Brandon Williams <brando5393@gmail.com>
2025-05-23 01:55:21 +0000

    update coffeebutton.jsx

--- src/components/CoffeeButton/CoffeeButton.jsx
+++ src/components/CoffeeButton/CoffeeButton.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const CoffeeButton = () => {
-  return (
-    <div>
-      <a href="https://paypal.me/brandontwilliams5393?locale.x=en_US">
-        <img
-          className="coffee-img"
-          src="../../public/Buymeacoffeeimagetransparent.png"
-          alt="Like my app? Buy me a coffee."
-        />
-      </a>
-    </div>
-  );
-};
+const CoffeeButton = () => (
+  <div>
+    <a href="https://paypal.me/brandontwilliams5393?locale.x=en_US">
+      <img
+        className="coffee-img"
+        src="/Buymeacoffeeimagetransparent.png"
+        alt="Like my app? Buy me a coffee."
+      />
+    </a>
+  </div>
+);
 
 export default CoffeeButton;
