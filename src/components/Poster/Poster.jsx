f5b7bd7d5395b60e991c47d74284291574001a5f
Brandon Williams <brando5393@gmail.com>
2025-05-23 01:58:38 +0000

    update poster.jsx

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-const Poster = (props) => {
-  return (
-    <div className="card">
-      <h5 className="card-header">Poster</h5>
-      <div className="card-body text-center">
-        <img
-          className="poster-img"
-          src={props.posterImg}
-          alt={`Poster for the film ${props.title}`}
-        />
-      </div>
+const Poster = ({ posterImg, title }) => (
+  <div className="card">
+    <h5 className="card-header">Poster</h5>
+    <div className="card-body text-center">
+      <img
+        className="poster-img"
+        src={posterImg}
+        alt={`Poster for the film ${title}`}
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default Poster;
